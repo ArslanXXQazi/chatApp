1fdd5ace157e45f31d17d8d2bccb88dacd992c77
Arslan Qazi <arslanqazi1999@gmail.com>
2026-06-04 18:58:37 +0500

    Update auth_controller.dart

--- lib/src/controllers/auth_controller.dart
+++ lib/src/controllers/auth_controller.dart
@@ -15,9 +15,26 @@ class AuthController extends GetxController {
   String? validateEmail(String value ){
     if(value==null || value.trim().isEmpty)
       {
-        return "Email is requried";
+        return "Email is Required";
       }
+    if(GetUtils.isEmail(value.trim()))
+      {
+        return "Enter Valid Email";
+      }
+    return null;
   }
+
+  String? validatePassword(String value){
+
+    if(value==null || value.isEmpty)
+      {
+        return "Enter Password";
+      }
+    if(value.length < 6)
+
+  }
+
+
 
 
   void togglePasswordVisibility (){
