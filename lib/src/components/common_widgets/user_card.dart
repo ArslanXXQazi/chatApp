5ed462e6af8349b26805101db8f7f7e02acd1e97
Arslan Qazi <arslanqazi1999@gmail.com>
2026-06-04 22:15:10 +0500

    Update user_card.dart

--- lib/src/components/common_widgets/user_card.dart
+++ lib/src/components/common_widgets/user_card.dart
@@ -11,37 +11,40 @@ class UserCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Responsive.init(context);
-    return Container(
-      padding: const EdgeInsetsGeometry.symmetric(horizontal: 20,vertical: 12),
-      margin: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            blurRadius: 5,
-            spreadRadius: 3,
-            offset: Offset(0, 2),
-          ),
-        ],
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Row(children: [
-        CircleAvatar(
-          radius: Responsive.width(25),
-          backgroundColor: Colors.purpleAccent,
-          child: Icon(Icons.person,color: Colors.white,size: Responsive.width(30),),
+    return GestureDetector(
+      onTap: (){},
+      child: Container(
+        padding: const EdgeInsetsGeometry.symmetric(horizontal: 20,vertical: 12),
+        margin: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              blurRadius: 5,
+              spreadRadius: 3,
+              offset: Offset(0, 2),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(12),
         ),
-        SizedBox(width: Responsive.width(15)),
-        Expanded(
-          child: BlackText(
-            text:name,
-            fontSize: 16,
-            fontWeight: FontWeight.w700,
-
+        child: Row(children: [
+          CircleAvatar(
+            radius: Responsive.width(25),
+            backgroundColor: Colors.purpleAccent,
+            child: Icon(Icons.person,color: Colors.white,size: Responsive.width(30),),
           ),
-        )
-      ],),
+          SizedBox(width: Responsive.width(15)),
+          Expanded(
+            child: BlackText(
+              text:name,
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+
+            ),
+          )
+        ],),
+      ),
     );
   }
 }
