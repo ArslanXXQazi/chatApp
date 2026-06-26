414ed4f839faf616c1ce05059acaacf8218e5a3f
Arslan Qazi <arslanqazi1999@gmail.com>
2026-06-26 15:24:25 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 
-import 'package:chatapp/src/router/app
+import 'package:chatapp
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
